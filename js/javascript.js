a872c7cb96a6c400d6a8bd305c19b2914bbbb103
Boris Pavel <boris.pavel@gmx.com>
2023-04-03 19:22:16 +0300

    Add remove function

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,11 @@
 const library = [];
+let bookId = 0;
 const booksContainer = document.querySelector(".books-container");
+const addButton = document.querySelector(".button");
+const closeButton = document.querySelector(".close");
+const bookForm = document.querySelector(".book-form");
+const submitForm = document.querySelector('button[type="submit"');
+
 
 function displayCard(book) {
   const card = document.createElement("div");
@@ -22,7 +28,19 @@ function displayCard(book) {
   const btns = document.createElement("div");
   const read = document.createElement("div");
   const remove = document.createElement("div");
-  read.textContent = "Read";
+  const id = document.createElement("div");
+  const idInfo = document.createElement("div");
+  idInfo.classList.add("idInfo");
+  id.textContent = "ID: ";
+  idInfo.textContent = book.id;
+  id.append(idInfo);
+  if (book.status) {
+    read.textContent = "Read";
+    read.classList.add("green");
+  } else {
+    read.textContent = "Not read";
+    read.classList.add("red");
+  }
   remove.textContent = "Remove";
   read.classList.add("read");
   remove.classList.add("remove");
@@ -32,19 +50,83 @@ function displayCard(book) {
   card.append(title);
   card.append(author);
   card.append(pages);
+  card.append(id);
   card.append(btns);
+
   booksContainer.append(card);
 }
-function displayLibrary() {
-  library.forEach((el) => displayCard(el));
+
+function addBookToLibrary(book) {
+  library.push(book);
+  displayCard(book);
 }
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.status = status;
+  this.id = bookId;
+  bookId += 1;
 }
 
-const alo = new Book("lol", "lol", 34);
-library.push(alo);
+Book.prototype.switchReadBtn = function () {
+  if (this.status) {
+    this.status = false;
+  } else this.status = true;
+};
 
-displayLibrary(library);
+addButton.addEventListener("click", () => {
+  bookForm.classList.toggle("hide");
+});
+
+closeButton.addEventListener("click", () => {
+  bookForm.classList.toggle("hide");
+});
+
+submitForm.addEventListener(
+  "click",
+  (e) => {
+    bookForm.classList.toggle("hide");
+    e.preventDefault();
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const yes = document.querySelector("#yes");
+    let status;
+    if (yes.checked) {
+      status = true;
+    } else {
+      status = false;
+    }
+    const book = new Book(title.value, author.value, pages.value, status);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    addBookToLibrary(book);
+  },
+  false
+);
+
+function switchReadBtn(node) {
+  node.classList.toggle("green");
+  node.classList.toggle("red");
+
+  if (node.textContent === "Read") {
+    node.textContent = "Not read";
+  } else {
+    node.textContent = "Read";
+  }
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("read")) {
+    switchReadBtn(e.target);
+  }
+  if (e.target.classList.contains("remove")) {
+    e.target.parentNode.parentNode.remove();
+    const bookIdCard =
+      e.target.parentNode.parentNode.querySelector(".idInfo").textContent;
+    const index = library.findIndex((el) => el.id === bookIdCard);
+    library.splice(index,1);
+  }
+});
